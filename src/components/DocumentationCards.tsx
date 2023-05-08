36c148cac7a6ecea92a60b2b77f89e8592c6c79f
Chloe Wu <chloewu21@gmail.com>
2023-05-08 15:21:17 -0700

    updated documentation cards

--- src/components/DocumentationCards.tsx
+++ src/components/DocumentationCards.tsx
@@ -1,24 +1,26 @@
 import styles from '../styles/Cards.module.css'
 import { FaGithub, FaDiscord } from 'react-icons/fa'
 import { BsBook, BsInfoCircle } from 'react-icons/bs'
+import { HiOutlineCurrencyDollar } from "react-icons/hi"
+import {FaMoneyCheckAlt} from 'react-icons/fa'
 
 export default function DocumentationCards() {
   return (
     <div className={styles.grid}>
-      <a href="https://openexa.com/" className={styles.card}>
+      <a href="https://openexa.ai/" target="_blank" className={styles.card}>
         <div className={styles.row}>
-          <BsBook />↗
+          <BsInfoCircle className={styles.logo} />↗
         </div>
-        <h3>What is OpenExa</h3>
-        <p>Learn about crypto markets and credit tokens.</p>
+        <h3>RWA Market Intelligence</h3>
+        <p>Real World Assets (RWA) - Market Research and Cryptocurrency Exploration.</p>
       </a>
 
-      <a href="https://github.com/OpenData-BondIntelligence" className={styles.card}>
+      <a href="https://token-interface-next-app-wmn5n7rc5q-uc.a.run.app/transactions" target="_blank" className={styles.card}>
         <div className={styles.row}>
-          <FaGithub className={styles.logo} />↗
+          <FaMoneyCheckAlt className={styles.logo} />↗
         </div>
-        <h3>Github</h3>
-        <p>View the OpenExa Github.</p>
+        <h3>OpenEXA Token Management</h3>
+        <p>Minting of AUT's and OXA's and Management of Tokens.</p>
       </a>
 
 
